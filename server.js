d4466853f1351e46374697243bf31fe2086ac297
Tony Han <itony9401@live.com>
2018-10-24 14:40:59 -0700

    removed unnecessaray import

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const errorhandler = require('errorhandler');
