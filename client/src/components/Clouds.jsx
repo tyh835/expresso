101adfe43285585fec43763469d5f0cd9a2feb56
Tony Han <itony9401@live.com>
2018-10-24 22:41:56 -0700

    refactored clouds to scss

--- client/src/components/Clouds.jsx
+++ client/src/components/Clouds.jsx
@@ -1,89 +1,39 @@
-import styled from 'styled-components';
-
-import { moveclouds } from '../utils/animations.js';
+import React from 'react';
+import './Clouds.scss';
 
 // Floating Clouds LOL
-const Cloud = styled.div`
-  width: 350px;
-  height: 120px;
-  background-color: #fff;
-  background: linear-gradient(top, #fefefe, 100%);
-  border-radius: 100px;
-  position: absolute;
-  margin: 120px auto 20px;
-  z-index: 1;
-  transition: all 1s ease;
-  pointer-events: none;
+export const Cloud1a = () => {
+  return (
+    <div id="Cloud1a" />
+  )
+};
 
-  &:after,
-  &:before {
-    content: '';
-    position: absolute;
-    background: #fff;
-    z-index: 3;
-  }
+export const Cloud1b = () => {
+  return (
+    <div id="Cloud1b" />
+  )
+};
 
-  &:after {
-    width: 100px;
-    height: 100px;
-    top: -50px;
-    left: 50px;
-    border-radius: 100px;
-  }
+export const Cloud2 = () => {
+  return (
+    <div id="Cloud2" />
+  )
+};
 
-  &:before {
-    width: 180px;
-    height: 180px;
-    top: -90px;
-    right: 50px;
-    border-radius: 200px;
-  }
-`;
+export const Cloud3 = () => {
+  return (
+    <div id="Cloud3" />
+  )
+};
 
-export const Cloud1a = styled(Cloud)`
-  top: 30px;
-  left: 100px;
-  transform: scale(0.3);
-  opacity: 0.55;
-  animation: ${moveclouds} 15s linear infinite;
-`;
+export const Cloud4 = () => {
+  return (
+    <div id="Cloud4" />
+  )
+};
 
-export const Cloud1b = styled(Cloud)`
-  top: 0px;
-  left: 250px;
-  transform: scale(0.3);
-  opacity: 0.65;
-  animation: ${moveclouds} 15s linear infinite;
-`;
-
-export const Cloud2 = styled(Cloud)`
-  left: 250px;
-  top: 100px;
-  transform: scale(0.6);
-  opacity: 0.3;
-  animation: ${moveclouds} 25s linear infinite;
-`;
-
-export const Cloud3 = styled(Cloud)`
-  left: 250px;
-  bottom: -70px;
-  transform: scale(0.6);
-  opacity: 0.5;
-  animation: ${moveclouds} 25s linear infinite;
-`;
-
-export const Cloud4 = styled(Cloud)`
-  left: 470px;
-  bottom: 50px;
-  transform: scale(0.75);
-  opacity: 0.4;
-  animation: ${moveclouds} 18s linear infinite;
-`;
-
-export const Cloud5 = styled(Cloud)`
-  left: 200px;
-  top: 300px;
-  transform: scale(0.5);
-  opacity: 0.5;
-  animation: ${moveclouds} 20s linear infinite;
-`;
+export const Cloud5 = () => {
+  return (
+    <div id="Cloud5 "/>
+  )
+};
