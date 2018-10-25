e5493a8c44b00755accdabdbd7f3447e09a4f8d2
Tony Han <itony9401@live.com>
2018-10-24 22:53:33 -0700

    refactored clouds

--- client/src/components/Clouds.jsx
+++ client/src/components/Clouds.jsx
@@ -2,38 +2,9 @@ import React from 'react';
 import './Clouds.scss';
 
 // Floating Clouds LOL
-export const Cloud1a = () => {
-  return (
-    <div id="Cloud1a" />
-  )
-};
-
-export const Cloud1b = () => {
-  return (
-    <div id="Cloud1b" />
-  )
-};
-
-export const Cloud2 = () => {
-  return (
-    <div id="Cloud2" />
-  )
-};
-
-export const Cloud3 = () => {
-  return (
-    <div id="Cloud3" />
-  )
-};
-
-export const Cloud4 = () => {
-  return (
-    <div id="Cloud4" />
-  )
-};
-
-export const Cloud5 = () => {
-  return (
-    <div id="Cloud5 "/>
-  )
-};
+export const Cloud1a = () => <div id="Cloud1a" />;
+export const Cloud1b = () => <div id="Cloud1b" />;
+export const Cloud2 = () => <div id="Cloud2" />;
+export const Cloud3 = () => <div id="Cloud3" />;
+export const Cloud4 = () => <div id="Cloud4" />;
+export const Cloud5 = () => <div id="Cloud5 "/>;
