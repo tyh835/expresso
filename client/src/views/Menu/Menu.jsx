8e66871833c96eee2b52a7a1fd4d999f485a7e3a
Tony Han <itony9401@live.com>
2018-10-25 21:21:03 -0700

    fixed menu item name sorting with empty string

--- client/src/views/Menu/Menu.jsx
+++ client/src/views/Menu/Menu.jsx
@@ -69,6 +69,12 @@ class Menu extends Component {
 
   sortMenuItems(menuItems) {
     return menuItems.sort((menuItem1, menuItem2) => {
+      if (!menuItem1.name) {
+        return -1;
+      }
+      if (!menuItem2.name) {
+        return -1;
+      }
       if (menuItem1.name < menuItem2.name) {
         return -1;
       } else {
