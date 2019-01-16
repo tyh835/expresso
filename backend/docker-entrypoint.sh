41c0a4317615436b346d4d78d0b5a1b169862ba5
Tony Han <itony9401@live.com>
2019-01-15 22:43:31 -0800

    got docker-compose to work

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 set -e
 
-while ! nc -z postgres 5432; do
-    echo "waiting for postgres..."
-    sleep 1;
-done
+# while ! nc -z postgres 5432; do
+#     echo "waiting for postgres..."
+#     sleep 1;
+# done
+
+yarn migrate
 
 if [ "$NODE_ENV" != prod ]; then
-  yarn migrate
+  yarn seed
 fi
+
+echo entrypoint success!
 
 exec "$@"
