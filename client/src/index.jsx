a598511d10f447168837a977ae431ac3b981f98b
Tony Han <itony9401@live.com>
2019-02-01 00:20:33 -0800

    formatting

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,6 +9,7 @@ import rootReducer from './reducers';
 import './styles/normalize.scss';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(thunk))
